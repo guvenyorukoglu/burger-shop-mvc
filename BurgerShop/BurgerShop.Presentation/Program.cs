c8e6ae7c17c7d291fa651c18da776f656fcfdcfd
Güven Yörükoğlu <yorukoglu.guven@gmail.com>
2023-11-28 19:00:59 +0300

    .

--- BurgerShop/BurgerShop.Presentation/Program.cs
+++ BurgerShop/BurgerShop.Presentation/Program.cs
@@ -33,7 +33,7 @@ namespace BurgerShop.Presentation
 
 
 
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("TarikSQL")));
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GuvenSQL")));
             builder.Services.AddTransient<IBaseRepository<Address>, AddressRepository>()
                             .AddTransient<IBaseRepository<AppUser>, AppUserRepository>()
                             .AddTransient<IBaseRepository<Extra>, ExtraRepository>()
